7408a586fa85901d22bfa05e9b04b2de9178ef9a
Ramon Audain <raudain123@gmail.com>
2020-06-01 16:25:34 -0700

    Working on homepage

--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -1,20 +1,18 @@
-import React from 'react'
-import "../css/App.css"
+import React from "react";
 
 const Header = () => {
-
-    return(
-        <div className="header">
-            <div className="left">
-                <p>Sauti.</p>
-            </div>
-            <div className="right">
-                <a href="">Register</a>
-                <a href="">Login</a>
-                <a href="">About</a>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <div className="left">
+        <p>Sauti.</p>
+      </div>
+      <div className="right">
+        <a href="">Register</a>
+        <a href="">Login</a>
+        <a href="">About</a>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
--- Frontend/src/components/Homepage.js
+++ Frontend/src/components/Homepage.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import Header from "./Header"
+import React from "react";
+import Header from "./Header";
 
 const Homepage = () => {
-    
-    return(
-        <>
-            <Header />
-            
-        </>
-    )
-}
+  return (
+    <>
+      <Header />
+      <div className="landingPage">
+          <div className="box">
+              <p>Mobile-based trade and market information</p>
+              <p>Empowering East Africa's women led SMEs to trade legally and across borders</p>
+          </div>
+      </div>
+    </>
+  );
+};
 
 export default Homepage;
