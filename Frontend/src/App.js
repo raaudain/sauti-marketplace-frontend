03a9e8b3ec547daac5029275d8047dbdc90e976a
Ramon Audain <raudain123@gmail.com>
2020-06-02 15:27:04 -0700

    Add routes for links

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import './css/App.css';
-import Homepage from "./components/Homepage"
+import React from "react";
+import {Route} from "react-router-dom";
+import Homepage from "./components/Homepage";
+import About from "./components/About";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Services from "./components/Services";
 
 const App = () => {
   return (
     <div className="container">
-      <Homepage />
+      <Route exact path="/" component={Homepage} />
+      <Route path="/about" component={About} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+      <Route path="/services" component={Services} />
     </div>
   );
-}
+};
 
 export default App;
