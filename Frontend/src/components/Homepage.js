26d679e8469abecbd25d935b5c92b47b71235d09
Ramon Audain <raudain123@gmail.com>
2020-06-03 18:36:17 -0700

    Formatting

--- Frontend/src/components/Homepage.js
+++ Frontend/src/components/Homepage.js
@@ -1,20 +1,27 @@
 import React from "react";
 import Header from "./Header";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
   return (
     <>
       <Header />
       <div className="landingPage">
-          <div className="box">
-              <p>Mobile-based trade and market information</p>
-              <p>Empowering East Africa's women led SMEs to trade legally and across borders</p>
-              <div>
-                  <Link to="/services"><button>Our Services</button></Link>
-                  <Link to="/about"><button>Our Impact</button></Link>
-              </div>
+        <div className="box">
+          <p>Mobile-based trade and market information</p>
+          <p>
+            Empowering East Africa's women led SMEs to trade legally and across
+            borders
+          </p>
+          <div>
+            <Link to="/services">
+              <button>Our Services</button>
+            </Link>
+            <Link to="/about">
+              <button>Our Impact</button>
+            </Link>
           </div>
+        </div>
       </div>
     </>
   );
