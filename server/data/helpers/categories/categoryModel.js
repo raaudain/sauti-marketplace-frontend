ecd93b2c6ba35bc170ff1cd7f31e61e712b8bf66
Ramon Audain <raudain123@gmail.com>
2020-06-18 13:36:45 -0700

    Set up category model

---
+++ server/data/helpers/categories/categoryModel.js
@@ -0,0 +1,39 @@
+module.exports = {
+  getCategories,
+  getCategory,
+  getCategoryLocation,
+  getCategoryLocationAverage,
+  getTest
+};
+
+function getCategories() {
+  return db("category").select("id", "category", "imgUrl");
+}
+
+function getCategory(id) {
+  return db("category").select("category", "imgURL").where({ id }).first();
+}
+
+function getCategoryLocation(id) {
+  return db("category")
+    .select("*")
+    .join("location", "category.id", "location.category_id")
+    .where("location.category", id);
+}
+
+function getCategoryLocationAverage(id) {
+  return db("category")
+    .select("*")
+    .join("location", "category.id", "location.category_id")
+    .join("items", "location.id", "items.country_id")
+    .where("location.category_id", id);
+}
+
+function getTest(id) {
+  return db("category")
+    .select("*")
+    .join("location", "category.id", "location.category_id")
+    .where("category.id", id);
+}
+
+const db = require("../../dbConfig");
