2c689027472c73bbe03779307dcf94e20b41b041
Ramon Audain <raudain123@gmail.com>
2020-06-20 21:52:44 -0700

    Set up locationModel

---
+++ server/data/helpers/location/locationModel.js
@@ -0,0 +1,34 @@
+module.exports = {
+  getCountries,
+  getCountry,
+  getAverage,
+};
+
+const db = requre("../../dbConfig.js");
+
+function getCountries() {
+  return db("location")
+    .orderBy("country");
+}
+
+function getCountry() {
+  return db("location")
+    .select("country")
+    .where({ id })
+    .first();
+}
+
+function getAverage(id) {
+  return db("location as loc")
+    .select(
+      "loc.id as country_id",
+      "loc.country",
+      "cat.id as category_id",
+      "i.id as item_id",
+      "i.item_name",
+      "i.item_price"
+    )
+    .join("category as cat", "loc.id", "cat.location_id")
+    .join("items as i", "cat.id", "i.c_id")
+    .where("cat.id", id);
+}
