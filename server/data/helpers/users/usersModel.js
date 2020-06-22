b6483e55a5713945092184609323452fb59f8c18
Ramon Audain <raudain123@gmail.com>
2020-06-21 17:28:06 -0700

    Set up usersModel

---
+++ server/data/helpers/users/usersModel.js
@@ -0,0 +1,45 @@
+module.exports = {
+  getUsers,
+  getUser,
+  addUser,
+  findUser,
+  updateUser,
+  removeUser
+};
+
+const db = require("../../dbConfig");
+
+function getUser(id) {
+  return db("users")
+    .select("id", "username", "profile_img")
+    .orderBy("username");
+}
+
+function addUser(user) {
+  return db("users")
+    .insert(user, "id")
+    .then(ids => {
+      const [id] = ids;
+
+      return db("users")
+        .where({ id })
+        .first();
+    });
+}
+
+function findUser(user) {
+  return db("users")
+    .where(user);
+}
+
+function updateUser(id, changes) {
+  return db("users")
+    .where({ id })
+    .update(changes);
+}
+
+function removeUser(id) {
+  return db("users")
+    .where("id", id)
+    .del();
+}
