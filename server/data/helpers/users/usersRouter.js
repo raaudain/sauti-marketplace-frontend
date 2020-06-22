ec6ef9973544d7ff94dbb7f546d994170008c65f
Ramon Audain <raudain123@gmail.com>
2020-06-21 17:50:29 -0700

    Set up usersRouter

---
+++ server/data/helpers/users/usersRouter.js
@@ -0,0 +1,69 @@
+const router = require("express").Router();
+
+const Users = require("./usersModel");
+const authenticate = require("../../../auth/middleware/authenticate");
+const validate = require("../middleware/validate");
+
+// Renders users
+router.get("/", (req, res) => {
+  Users
+    .getUsers()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(`${err}`);
+    });
+});
+
+// Renders specific user
+router.get("/:id", validate.validateUserId, (req, res) => {
+  const { id } = req.params;
+
+  Users
+    .getUser(id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(`${err}`);
+    });
+});
+
+// Updates user information
+router.put("/:id", authenticate, validate.validateUserId, (req, res) => {
+  const { id } = req.params;
+  const change = req.body;
+
+  Users
+    .getUser(id)
+    .then(() => {
+      return Users.updateUser(id, change)
+        .then(() => {
+          res.status(200).json({ message: "User data updated" });
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json({ message: `${err}` });
+        });
+  });
+});
+
+// Deletes user
+router.delete("/:id", authenticate, validate.validateUserId, (req, res) => {
+  const { id } = req.params;
+
+  Users
+    .removeUser(id)
+    .then(() => {
+      res.status(200).json({ message: "User removed" });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: `${err}` });
+    });
+});
+
+module.exports = router;
