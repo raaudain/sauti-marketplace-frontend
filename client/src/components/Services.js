4ac4a71c38a2c8c89163864edecab3b69856eab6
Ramon Audain <raudain123@gmail.com>
2020-06-26 18:30:03 -0700

    Style Services

--- client/src/components/Services.js
+++ client/src/components/Services.js
@@ -5,9 +5,11 @@ const Services = () => {
   return (
     <>
       <Header />
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a massa orci. Pellentesque porta posuere magna, ac consectetur ipsum dapibus at. Curabitur tristique nisi est, sed interdum libero accumsan a. Aenean tristique mauris et auctor mollis. Mauris euismod velit massa, quis molestie orci venenatis in. Etiam non justo at nisl ultricies lobortis id in dolor. Aenean a volutpat quam. </p>
-      <p>Mauris dapibus ligula orci, vitae porttitor tellus elementum a. Mauris sit amet eros at sapien commodo imperdiet. Duis dictum lorem quis dolor suscipit, ac rutrum neque elementum. Aliquam pretium convallis tortor, condimentum iaculis mi. Donec tempor ultrices mi faucibus porttitor. Mauris erat risus, facilisis ut mollis vitae, faucibus in sem. Maecenas dolor ligula, luctus eu ex in, imperdiet tincidunt risus. Quisque dapibus ultricies venenatis. Vestibulum sodales quam ut justo aliquet tempor.</p>
-      <p>Etiam ornare ultrices dignissim. Nulla iaculis mauris a mi viverra venenatis. Aliquam lobortis urna vel lacus dictum, a pulvinar tellus vehicula. Maecenas quis pulvinar orci. Nulla vel dui magna. Vivamus tempus feugiat vestibulum. Cras vestibulum lectus a erat varius, eu tempus tortor posuere. Integer ut mi ut justo tincidunt feugiat ut sit amet est. Phasellus nec convallis risus. In ipsum nulla, tristique vitae maximus ac, tempor et arcu.</p>
+      <div id="service-lorem">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a massa orci. Pellentesque porta posuere magna, ac consectetur ipsum dapibus at. Curabitur tristique nisi est, sed interdum libero accumsan a. Aenean tristique mauris et auctor mollis. Mauris euismod velit massa, quis molestie orci venenatis in. Etiam non justo at nisl ultricies lobortis id in dolor. Aenean a volutpat quam. </p>
+        <p>Mauris dapibus ligula orci, vitae porttitor tellus elementum a. Mauris sit amet eros at sapien commodo imperdiet. Duis dictum lorem quis dolor suscipit, ac rutrum neque elementum. Aliquam pretium convallis tortor, condimentum iaculis mi. Donec tempor ultrices mi faucibus porttitor. Mauris erat risus, facilisis ut mollis vitae, faucibus in sem. Maecenas dolor ligula, luctus eu ex in, imperdiet tincidunt risus. Quisque dapibus ultricies venenatis. Vestibulum sodales quam ut justo aliquet tempor.</p>
+        <p>Etiam ornare ultrices dignissim. Nulla iaculis mauris a mi viverra venenatis. Aliquam lobortis urna vel lacus dictum, a pulvinar tellus vehicula. Maecenas quis pulvinar orci. Nulla vel dui magna. Vivamus tempus feugiat vestibulum. Cras vestibulum lectus a erat varius, eu tempus tortor posuere. Integer ut mi ut justo tincidunt feugiat ut sit amet est. Phasellus nec convallis risus. In ipsum nulla, tristique vitae maximus ac, tempor et arcu.</p>
+      </div>
     </>
   );
 };
